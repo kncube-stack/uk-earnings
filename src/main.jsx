35a95f78511a1ec76eb99437845cbcf3a92a6c0d
Kennedy Ncube <ken.ncube@icloud.com>
2026-03-09 15:20:40 +0000

    Add Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import EarningsDashboard from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <EarningsDashboard />
+    <Analytics />
   </React.StrictMode>
 )
